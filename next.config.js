e8d1f6c7f4667867ffa425d55e6dce815926749e
hendrywilliam <hendriwilliam29@gmail.com>
2023-02-03 16:22:22 +0700

    lib: redux supabase initialization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    SUPABASE_URL: process.env.URL,
+    SUPABASE_KEY: process.env.KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
